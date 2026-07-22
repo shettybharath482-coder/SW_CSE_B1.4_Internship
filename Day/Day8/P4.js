7ec511c1d25aae48c1a3d724ce5a581bead25922
shettybharath482-coder <shettybharath482@gmail.com>
2026-07-22 12:54:05 +0530

    let keyword based variable

--- Day/Day8/P4.js
+++ Day/Day8/P4.js
@@ -9,3 +9,19 @@ if (true) {
 }
 // var in JS is not block-scoped
 console.log(message);
+console.log(message);
+
+// ****************************************************
+// let keyword based variable
+let score = 100;
+//let score = 100; //SyntaxError: Identifier 'score' has already been declared
+score = 160; // re-assignment is valid
+console.log(score)
+
+let secret; // Declare outside the block
+
+if (true) {
+    secret = "Password@123"; // Assign inside the block
+}
+
+console.log(secret); // Password@123
