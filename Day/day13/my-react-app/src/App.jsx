e92c6f1ea49df81c4ab2ac344982b616fee17801
shettybharath482-coder <shettybharath482@gmail.com>
2026-07-29 15:35:20 +0530

    Event Handling ex2

--- Day/day13/my-react-app/src/App.jsx
+++ Day/day13/my-react-app/src/App.jsx
@@ -11,6 +11,8 @@ import Counter from "./components/State";
 import ToggleText from "./components/StateTwo";
 import NameInput from "./components/StateThree";
 import ClickDemo from "./components/EventHandling";
+import FormDemo from "./components/EventHandlingTwo";
+
 const name = "Amay";
 const isLoggedIn = false;
 
@@ -50,6 +52,7 @@ function App() {
 
       <NameInput />
       <ClickDemo/>
+      <FormDemo/>
       
     </>
  
