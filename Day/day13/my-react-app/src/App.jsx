0669b15b83f5fa9b3776e860b94ccd7102bc7d13
shettybharath482-coder <shettybharath482@gmail.com>
2026-07-29 11:41:38 +0530

    Imported the components and rendered it Default export from component Named export from component

---
+++ Day/day13/my-react-app/src/App.jsx
@@ -0,0 +1,35 @@
+import Button from "./components/FunctionalComponent";
+import { LeftArrow, RightArrow } from "./components/FunctionalComponentTwo";
+
+const name = "Amay";
+const isLoggedIn = false;
+
+function App() {
+  return (
+    // React Fragments
+    <>
+      {/* JSX Rule 1: Return one parent element */}
+      <h1>Hello</h1>
+      <p>Hello again</p>
+
+      {/* JSX Rule 2: Every tag must be closed properly */}
+      <p>Error</p>
+      <input type="text" />
+
+      {/* JSX Rule 3: use camelCase for attributes */}
+      <button>Click</button>
+
+      {/* JSX Rule 4: Embedding JavaScript with curly braces */}
+      <h3>Hello, {name}</h3>
+
+      {/* JSX Rule 5: use expressions, not statements */}
+      <p>{isLoggedIn ? "Welcome back" : "Please Login"}</p>
+
+      <Button />
+      <LeftArrow />
+      <RightArrow />
+    </>
+  );
+}
+
+export default App;
---
+++ Day/day13/my-react-app/src/components/FunctionalComponentTwo.jsx
@@ -0,0 +1,6 @@
+// Named export
+
+export const LeftArrow = () => <span>⬅️</span>;
+export const RightArrow = () => <span>➡️</span>;
+
+// Windows + . : emojis
