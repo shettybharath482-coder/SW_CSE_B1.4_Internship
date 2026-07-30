5d2b20afddd0a2025a3b8334f08ec8b9bdb0ff1d
shettybharath482-coder <shettybharath482@gmail.com>
2026-07-30 15:41:53 +0530

    Form ex2

--- Day/Day14/day14/src/App.jsx
+++ Day/Day14/day14/src/App.jsx
@@ -6,6 +6,7 @@ import CounterHook from "./components/HookOne";
 import UserListHook from "./components/HookTwo";
 import ThemeProviderDemo from "./components/HookThree";
 import SimpleNameForm from "./components/FormOne";
+import SignupForm from "./components/FormTwo";
 
 function App() {
   
@@ -18,7 +19,8 @@ function App() {
       <CounterHook/> */}
       {/* <UserListHook/> */}
       {/* <ThemeProviderDemo /> */}
-      <SimpleNameForm />
+        {/* <SimpleNameForm /> */}
+      <SignupForm />
     </>
   )
 }
