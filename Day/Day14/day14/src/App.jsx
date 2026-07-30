285d5d2ac9db992c22995b22595822c6c6164737
shettybharath482-coder <shettybharath482@gmail.com>
2026-07-30 13:24:13 +0530

    Hooks: ex2 useEffect hook

--- Day/Day14/day14/src/App.jsx
+++ Day/Day14/day14/src/App.jsx
@@ -3,16 +3,18 @@ import SimpleList from "./components/ListOne";
 import UserList from "./components/ListTwo";
 import ProductListDemo from "./components/ListThree";
 import CounterHook from "./components/HookOne";
+import UserListHook from "./components/HookTwo";
 
 function App() {
   
   return (
     <>
-       {/* <SimpleList /> */}
-      <SimpleList />
+       <SimpleList />
+       { /* <SimpleList /> 
       <UserList />
       <ProductListDemo />
-      <CounterHook/>
+      <CounterHook/> */}
+      <UserListHook/> 
     </>
   )
 }
